27500f80a632e31948adc01db89f876034f09a69
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-02-22 20:12:24 +0100

    Initial commit

---
+++ src/main/java/com/example/simonogsofiebryllup/SimonOgSofieBryllupApplication.java
@@ -0,0 +1,13 @@
+package com.example.simonogsofiebryllup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonOgSofieBryllupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimonOgSofieBryllupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simonogsofiebryllup/SimonOgSofieBryllupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simonogsofiebryllup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonOgSofieBryllupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
